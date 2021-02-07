57949cd7fac59d5f355ae2976d3fee07c3099a6f
ThomasOreo <Thomas281204@gmail.com>
2021-02-08 00:29:28 +1000

    Update chroma crosshair.py

--- chroma crosshair.py
+++ chroma crosshair.py
@@ -1,3 +1,4 @@
+
 ##################################################################################################################################
 ###                                                                                                                            ###
 ###     :::::::::::   :::    :::    ::::::::      :::   :::     :::    :::    ::::::::     ::::::::    :::    :::   ::::::::   ###
@@ -13,12 +14,21 @@
 import telnetlib
 import time
 import colorsys
+
 HOST = "localhost"
 PORT = 8888
 delay = 0.05
-index = 0
+
 tn = telnetlib.Telnet(HOST, PORT)
+
+with open(__file__) as f:
+    for x in f.readlines()[:13]:
+        print(x.strip())
+        tn.write(('echo "' + x.strip() + '"\n').encode('utf-8'))
+
 tn.write("cl_crosshaircolor 5\n".encode('utf-8'))
+
+index = 0
 while True:
     r, g, b = tuple(round(i * 255) for i in colorsys.hsv_to_rgb(index/360,1,1))
     tn.write("cl_crosshaircolor_r {}\n".format(r).encode('utf-8'))
